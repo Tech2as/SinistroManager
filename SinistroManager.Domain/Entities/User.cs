8c4441d56890b5667434ff4c0ef9572325753991
Joao Vitor Guilherme Borges <joaoborges@MacBook-Air-de-Joao.local>
2026-02-26 18:56:29 -0300

    refactor: move User to Entities and update namespace

--- SinistroManager.Domain/Entities/User.cs
+++ SinistroManager.Domain/Entities/User.cs
@@ -1,14 +1,21 @@
+using SinistroManager.Domain.Enums;
+
 namespace SinistroManager.Domain.Entities;
-public abstract class User
+
+public class User
 {
+    public Guid Id { get; private set; }
     public string Name { get; private set; }
     public string Email { get; private set; }
-    public string CPF { get; private set; }
+    public string PasswordHash { get; private set; }
+    public UserRole Role { get; private set; }
 
-    protected User(string name, string email, string cpf)
+    public User(string name, string email, string passwordHash, UserRole role)
     {
+        Id = Guid.NewGuid();
         Name = name;
         Email = email;
-        CPF = cpf;
+        PasswordHash = passwordHash;
+        Role = role;
     }
 }
