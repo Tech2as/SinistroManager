3f75bf34fd0d37be3091f9828a2290aa35c88e6a
Joao Vitor Guilherme Borges <joaoborges@MacBook-Air-de-Joao.local>
2026-02-28 20:32:58 -0300

    feat: add ReguladorProfile entity

--- SinistroManager.Console/obj/Debug/net10.0/SinistroManager.Console.AssemblyInfo.cs
+++ SinistroManager.Console/obj/Debug/net10.0/SinistroManager.Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SinistroManager.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c4441d56890b5667434ff4c0ef9572325753991")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f35c19272779658cea5397d2857da3536df70e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("SinistroManager.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SinistroManager.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
