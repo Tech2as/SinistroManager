dfb028a9673502559c43f3b187b2a183940d18f0
Tech2as <95533385+Tech2as@users.noreply.github.com>
2026-03-02 19:20:15 -0300

    refactor: domain User interface

--- SinistroManager.Domain/Interfaces/IUserRepository.cs
+++ SinistroManager.Domain/Interfaces/IUserRepository.cs
@@ -1,11 +1,8 @@
 using SinistroManager.Domain.Entities;
 public interface IUserRepository
 {
-    public interface IUserRepository
-{
     Task<User?> GetByEmailAsync(string email);
     Task<User?> GetByIdAsync(Guid id);
     Task AddAsync(User user);
     Task SaveChangesAsync();
-}
 }
