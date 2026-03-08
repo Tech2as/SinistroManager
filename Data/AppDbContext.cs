177e2a7638c595e39d89da0bf64e79e98a8f68c3
Tech2as <techeletrum@gmail.com>
2026-03-08 17:54:54 -0300

    refactor: refine entity configurations

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -17,8 +17,12 @@ public class AppDbContext : DbContext
         modelBuilder.Entity<User>(e =>
         {
             e.HasKey(u => u.Id);
-            e.Property(u => u.Name).IsRequired().HasMaxLength(150);
-            e.Property(u => u.Email).IsRequired().HasMaxLength(200);
+            e.Property(u => u.Name)
+            .IsRequired()
+            .HasMaxLength(150);
+            e.Property(u => u.Email)
+            .IsRequired()
+            .HasMaxLength(200);
             e.HasIndex(u => u.Email).IsUnique();
             e.Property(u => u.Role).HasConversion<string>();
         });
@@ -26,8 +30,27 @@ public class AppDbContext : DbContext
         modelBuilder.Entity<Sinistro>(e =>
         {
             e.HasKey(s => s.Id);
-            e.Property(s => s.Chassi).IsRequired();
+
+            // id da oficina
+            e.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(s => s.OficinaId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            // id do regulador
+            e.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(s => s.ReguladorId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            e.Property(s => s.Chassi)
+            .IsRequired()
+            .HasMaxLength(17);
+            e.Property(s => s.ValorReparo).HasColumnType("decimal(18,2)");
+            e.Property(s => s.Salvado).IsRequired();
             e.Property(s => s.Status).HasConversion<int>();
+            e.Property(s => s.DataSinistro).IsRequired();
+
         });
     }
 }
